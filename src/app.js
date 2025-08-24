77349336901560cd1bf5dc43419b112cbdbca889
ugyentring <tsheringugyen483@gmail.com>
2025-08-25 02:41:27 +0600

    fix urlencoded extended:true bug fix

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(
 
 //express middlewares
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //cookie parser middleware
