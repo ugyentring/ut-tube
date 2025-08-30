26f42045e1e2eaa5bc5c5cadb31e47d67203f683
ugyentring <tsheringugyen483@gmail.com>
2025-08-30 21:36:05 +0600

    fix typoin the register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (req, re) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "ok",
   });
